a6d3829d7e6a2a5314540deeb211fb545cba8bc4
juliafealves <juliafealves>
2018-06-04 17:39:48 -0300

    Criada a classe de Aluno e os teste unitários.

---
+++ src/aluno/Aluno.java
@@ -0,0 +1,62 @@
+package aluno;
+
+import java.util.Objects;
+
+/**
+ * Representa um Aluno e seus atributos.
+ *
+ * @author Júlia Fernandes Alves (julia.alves@ccc.ufcg.edu.br)
+ */
+public class Aluno {
+
+    private String matricula;
+    private String nome;
+    private String curso;
+
+    /**
+     * Cria um objeto do tipo Aluno.
+     *
+     * @param matricula Matrícula do aluno
+     * @param nome Nome completo do aluno
+     * @param curso Curso do aluno
+     */
+    public Aluno(String matricula, String nome, String curso) {
+        if(matricula == null || matricula.isEmpty())
+            throw new IllegalArgumentException("Matrícula deve ser informada.");
+
+        if(nome == null || nome.isEmpty())
+            throw new IllegalArgumentException("Nome deve ser informado.");
+
+        if(curso == null || curso.isEmpty())
+            throw new IllegalArgumentException("Curso deve ser informado.");
+
+        this.matricula = matricula;
+        this.nome = nome;
+        this.curso = curso;
+    }
+
+    /**
+     * Verifica se dois objetos são iguais. Só serão iguais se tiverem a mesma matrícula.
+     *
+     * @param object
+     * @return
+     */
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Aluno aluno = (Aluno) object;
+
+        return Objects.equals(matricula, aluno.matricula);
+    }
+
+    /**
+     * Geração de hashCode baseado na matrícula do aluno.
+     *
+     * @return
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(matricula);
+    }
+}
--- test/aluno/AlunoTest.java
+++ test/aluno/AlunoTest.java
@@ -1,4 +1,107 @@
 package aluno;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Responsável por realizar testes unitários da classe Aluno.
+ *
+ * @author Júlia Fernandes Alves (julia.alves@ccc.ufcg.edu.br)
+ */
 public class AlunoTest {
+    private Aluno aluno;
+
+    @Before
+    public void criaAluno(){
+        this.aluno = new Aluno("789", "Alan Turing", "Matemática");
+    }
+
+    /**
+     * Teste para criação de aluno.
+     */
+    @Test
+    public void testAluno(){
+        new Aluno("123", "Albert Einstein", "Física");
+    }
+
+    /**
+     * Verifica se ocorre exceção ao criar um aluno com matrícula vazia.
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void testAlunoComMatriculaVazia(){
+        new Aluno("", "Isaac Newton", "Física");
+    }
+
+    /**
+     * Verifica se ocorre exceção ao criar um aluno com matrícula nula.
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void testAlunoComMatriculaNula(){
+        new Aluno(null, "Isaac Newton", "Física");
+    }
+
+    /**
+     * Verifica se ocorre exceção ao criar um aluno com nome vazio.
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void testAlunoComNomeVazio(){
+        new Aluno("123", "", "Física");
+    }
+
+    /**
+     * Verifica se ocorre exceção ao criar um aluno com nome nulo.
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void testAlunoComNomeNulo(){
+        new Aluno("123", null, "Física");
+    }
+
+    /**
+     * Verifica se ocorre exceção ao criar um aluno com curso vazio.
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void testAlunoComCursoVazio(){
+        new Aluno("123", "Isaac Newton", "");
+    }
+
+    /**
+     * Verifica se ocorre exceção ao criar um aluno com curso nulo.
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void testAlunoComCursoNulo(){
+        new Aluno("123", "Isaac Newton", null);
+    }
+
+    /**
+     * Verifica se dois objetos alunos são iguais. Eles serão ditos como iguais caso as matrículas sejam as mesmas.
+     */
+    @Test
+    public void testAlunosIguais(){
+        Assert.assertTrue(this.aluno.equals(new Aluno("789", "Sigmund Freud", "Medicina")));
+    }
+
+    /**
+     * Verifica se dois objetos alunos são diferentes.
+     */
+    @Test
+    public void testAlunosDiferentes(){
+        Assert.assertFalse(this.aluno.equals(new Aluno("123", "Sigmund Freud", "Medicina")));
+    }
+
+    /**
+     * Verifica se dois objetos alunos iguais possuem o mesmo hashCode.
+     */
+    @Test
+    public void testAlunoHashCodeIguais(){
+        Assert.assertEquals(this.aluno.hashCode(), new Aluno("789", "Sigmund Freud", "Medicina").hashCode());
+    }
+
+    /**
+     * Verifica se dois objetos alunos diferentes possuem hashCode diferentes.
+     */
+    @Test
+    public void testAlunoMesmoHashCodeDiferente(){
+        Assert.assertNotEquals(this.aluno.hashCode(), new Aluno("123", "Sigmund Freud", "Medicina").hashCode());
+    }
 }
